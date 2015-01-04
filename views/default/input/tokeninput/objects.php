c5b89ce4a2f100c8a75d411a88003ee1a01c7a2c
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-01-04 13:32:04 +0100

    Only append subtype to query if it's set

--- views/default/input/tokeninput/objects.php
+++ views/default/input/tokeninput/objects.php
@@ -4,14 +4,11 @@
  * Shortcut view to display objects picker
  * @uses $vars['subtype'] Subtype of objects to return
  */
-
 $vars['callback'] = 'elgg_tokeninput_search_objects';
 
 $subtype = elgg_extract('subtype', $vars, array());
-$vars['query'] = array(
-	'options' => array(
-		'subtypes' => $subtype,
-		)
-);
+if ($subtype) {
+	$vars['query']['options']['subtypes'] = $subtype;
+}
 
 echo elgg_view('input/tokeninput', $vars);
