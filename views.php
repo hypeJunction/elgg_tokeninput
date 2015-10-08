6012e9345342715fa1b4d932665345b70efed415
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-10-08 15:17:22 +0300

    fix(deps): fix autoloading

--- views.php
+++ views.php
@@ -1,14 +1,20 @@
 <?php
 
-$path = '';
+$plugin_root = __DIR__;
+$root = dirname(dirname($plugin_root));
+$alt_root = dirname(dirname(dirname($initial_root)));
+
 if (file_exists("$plugin_root/vendor/autoload.php")) {
-	// composer was run locally
-	$path = __DIR__ . '/';
+	$path = $plugin_root;
+} else if (file_exists("$root/vendor/autoload.php")) {
+	$path = $root;
+} else {
+	$path = $alt_root;
 }
 
 return [
 	'default' => [
-		'js/jquery.tokeninput.js' => $path . 'vendor/bower-asset/jquery-tokeninput/build/jquery.tokeninput.min.js',
+		'jquery.tokeninput.js' => $path . 'vendor/bower-asset/jquery-tokeninput/build/jquery.tokeninput.min.js',
 	]
 ];
 
