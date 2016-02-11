768a19c37ec142140d1fa44c19eead97d08c1ea7
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-02-11 15:05:09 +0100

    fix(views): fix variable name in views.php

--- views.php
+++ views.php
@@ -2,7 +2,7 @@
 
 $plugin_root = __DIR__;
 $root = dirname(dirname($plugin_root));
-$alt_root = dirname(dirname(dirname($initial_root)));
+$alt_root = dirname(dirname(dirname($root)));
 
 if (file_exists("$plugin_root/vendor/autoload.php")) {
 	$path = $plugin_root;
