82258dea577d1f597f489ea75f2ffabdd8f5b040
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-06-16 10:57:16 +0200

    fix(input): userpicker input behaviour now corresponds to core
    Adds additional logic to match userpicker uses in core. In particular, fixes
    an issue with using tokeninput with te messages plugin.
    
    Fixes #6

--- views/default/input/userpicker.php
+++ views/default/input/userpicker.php
@@ -1,7 +1,22 @@
 <?php
 
-$vars['name'] = 'members';
+if (empty($vars['name'])) {
+	$vars['name'] = 'members';
+}
+
 $vars['callback'] = 'elgg_tokeninput_search_users';
-$vars['multiple'] = true;
+
+$limit = elgg_extract('limit', $vars);
+if (!$limit || $limit > 1) {
+	$vars['multiple'] = true;
+} else {
+	$vars['multiple'] = false;
+	$vars['autoexplode'] = false;
+}
+
+if (isset($vars['values'])) {
+	$vars['value'] = elgg_extract('values', $vars);
+	unset($vars['values']);
+}
 
 echo elgg_view('input/tokeninput', $vars);
